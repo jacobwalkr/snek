5a883cc8ca3bae9a024f4b616f284892ea6a119f
Jacob Walker <jacob.walker94@gmail.com>
2016-04-27 01:01:25 +0200

    Move game loop to Game.rb and move most of code to lib dir
    Still just a square moving around the screen - but it's *my* square

---
+++ lib/game.rb
@@ -0,0 +1,65 @@
+include Curses
+
+class Game
+  def initialize(window, grid_y, grid_x)
+    @window = window
+    @grid_y = grid_y
+    @grid_x = grid_x
+  end
+
+  def play
+    game_thread = start_game
+
+    # Defaults to moving right at the start of the game
+    next_instruction = :right
+    loop do
+      next_char = Curses.getch
+
+      case next_char
+      when KEY_UP
+        next_instruction = :up
+      when KEY_DOWN
+        next_instruction = :down
+      when KEY_LEFT
+        next_instruction = :left
+      when KEY_RIGHT
+        next_instruction = :right
+      when 10 # The enter key
+        game_thread[:stop] = true
+        game_thread.join
+        break
+      end
+
+      game_thread[:next_instruction] = next_instruction
+    end
+  end
+
+  private
+    def start_game
+      thread = Thread.start do
+        Thread.current[:stop] = false
+        Thread.current[:next_instruction] = :right
+        snake = Snake.new(@grid_y / 2, @grid_x / 2)
+
+        loop do
+          break if Thread.current[:stop]
+
+          remove, add = snake.move(Thread.current[:next_instruction])
+
+          remove.each do |segment|
+            @window.setpos(segment.y, segment.x * 2)
+            @window.addstr('  ') # two spaces
+          end
+
+          add.each do |segment|
+            @window.setpos(segment.y, segment.x * 2)
+            @window.addstr(segment.sprite)
+          end
+
+          @window.refresh
+          sleep 0.1
+        end
+      end
+    end
+end
+
--- snek.rb
+++ snek.rb
@@ -1,5 +1,5 @@
 require "curses"
-require_relative "snake"
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 include Curses
 
@@ -14,62 +14,13 @@ begin
   Curses.curs_set 0
   Curses.noecho
   stdscr.keypad = true
-  window = Curses::Window.new 0, 0, 0, 0
+  window = Curses::Window.new(0, 0, 0, 0)
   window.nodelay = true
   grid_y = Curses.lines
   grid_x = Curses.cols / 2 # snake is two-wide to make each segment appear as a square
 
-  show_message(window, "Snake!")
-
-  thread = Thread.start do
-    Thread.current[:stop] = false
-    Thread.current[:next_instruction] = :right
-    snake = Snake.new(grid_y / 2, grid_x / 2)
-
-    loop do
-      break if Thread.current[:stop]
-
-      remove, add = snake.move(Thread.current[:next_instruction])
-
-      remove.each do |segment|
-        window.setpos(segment.y, segment.x * 2)
-        window.addstr('  ') # two spaces
-      end
-
-      add.each do |segment|
-        window.setpos(segment.y, segment.x * 2)
-        window.addstr(segment.sprite)
-      end
-
-      window.refresh
-      sleep 0.1
-    end
-  end
-
-  #window.addstr thread.status.to_s
-
-  # Defaults to moving right at the start of the game
-  next_instruction = :right
-  loop do
-    next_char = Curses.getch
-
-    case next_char
-    when KEY_UP
-      next_instruction = :up
-    when KEY_DOWN
-      next_instruction = :down
-    when KEY_LEFT
-      next_instruction = :left
-    when KEY_RIGHT
-      next_instruction = :right
-    when 10 # The enter key
-      thread[:stop] = true
-      thread.join
-      break
-    end
-
-    thread[:next_instruction] = next_instruction
-  end
+  game = Game.new(window, grid_y, grid_x)
+  game.play
 ensure
   Curses.close_screen
 end
